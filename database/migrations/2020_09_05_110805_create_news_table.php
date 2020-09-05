201dba19e736f736d5de8f11fa41bb2e9a162fbe
Artur <arturmostowiak@gmail.com>
2020-09-05 17:40:05 +0200

    rozbudowano_strone_glowna

---
+++ database/migrations/2020_09_05_110805_create_news_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('news_header',100)->nullable();
+            $table->string('news_content')->nullable();
+            $table->string('template_name',50)->default('default_news');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -1,3 +1,26 @@
 <x-base title="{{__('sex_empire_main_page')}}" specificImageClass="{{$specificImageClass}}" description="{{__('sex_empire_main_page_description')}}">
-
+   <main class="main-content">
+      <header class="introductory-content">
+         <h1 class="introductory-header">{{__('welcome')}}<span class="fas introductory-header-icon fa-grin-hearts"></span></h1>
+         <p class="news-introduction">{{__('news_introduction')}}</p>
+         <h2 class="quick-start-header">{{__('quick_start')}}<span class="fas quick-start-icon fa-play-circle"></span></h2>
+         <ul class="quickstart-options-list">
+             <li class="quickstart-option">
+                <span class="quickstart-option-description">{{__('pornstars')}}</span>
+                <span class="fas quickstart-option-icon fa-star"></span>
+            </li>
+            <li class="quickstart-option">
+                <span class="quickstart-option-description">{{__('categories')}}</span>
+                <span class="fas quickstart-option-icon fa-images"></span>
+            </li>
+            <li class="quickstart-option">
+                <span class="quickstart-option-description">{{__('search_engine')}}</span>
+                <span class="fas quickstart-option-icon  fa-search"></span>
+            </li>
+          </ul>
+      </header>
+      <div class="news-container">
+          <h2 class="news-header">{{__('news_header')}}</h2>
+      </div>
+   </main>
 </x-base>
