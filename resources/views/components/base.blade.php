09871ca4c72b0667e6355f31658feccb13085d13
Artur Mostek <arturmostowiak@gmail.com>
2021-10-29 13:21:37 +0200

    added_google_tag_menager

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -2,6 +2,13 @@
 <html lang="{{App::getLocale()}}">
 
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M2LQVFK');</script>
+<!-- End Google Tag Manager -->
 	<meta charset="UTF-8">
 	<title>{{$title}}</title>
 	<meta name="description" content="{{$description}}">
@@ -30,6 +37,10 @@
 </head>
 
 <body class="full-body @isset($customBodyClass){{$customBodyClass}}@endisset">
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2LQVFK"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 	<div class="aplication-container" id="app">
 		<navbar @auth v-bind:user-id="{{Auth::user()->id}}" user-name="{{Auth::user()->login}}" @if(Auth::user()->has_avatar)
 			initial-avatar-file-name="{{Auth::user()->avatar_file_name}}"
