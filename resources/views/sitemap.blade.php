c3d8b33f990c6b2bb45f553d993b1015c02e4a18
Artur Mostowiak <a.mostowiak@madkom.pl>
2021-12-08 18:46:53 +0100

    improved sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -19,43 +19,45 @@
       <changefreq>weekly</changefreq>
 </url>
 @endforeach
-<!-- movies  -->
 
- @foreach($movies as $movie)
-   <url>
-      <loc>{{$movie->getLink()}}</loc>
-      <changefreq>monthly</changefreq>
-   </url>
-@endforeach
-
-<url>
-      <loc>{{route('movies.new')}}</loc>
-      <changefreq>daily</changefreq>
-   </url>
-
-   <url>
-      <loc>{{route('movies.the-most-popular')}}</loc>
-      <changefreq>monthly</changefreq>
-   </url>
 
 <!-- pornstars -->
-<url>
-      <loc> {{route('pornstars.list')}}</loc>
-      <changefreq>monthly</changefreq>
-
-      @foreach($pornstars as $pornstar)
-         <image:image>
-            <image:loc>{{$pornstar->getImageURL()}}</image:loc>
-            <image:title>{{__('portrait_picture_of', ['nickname' => $pornstar->nickname])}}</image:title>
-         </image:image>
-      @endforeach
-</url>
-
 @foreach($pornstars as $pornstar)
-   <url>
-      <loc>{{ $pornstar->getProfileLink() }}</loc>
-      <changefreq>monthly</changefreq>
-   </url>
+<url>
+   <loc>{{ $pornstar->getProfileLink() }}</loc>
+   <changefreq>monthly</changefreq>
+</url>
 @endforeach
 
+<url>
+   <loc> {{route('pornstars.list')}}</loc>
+   <changefreq>monthly</changefreq>
+   
+   @foreach($pornstars as $pornstar)
+   <image:image>
+      <image:loc>{{$pornstar->getImageURL()}}</image:loc>
+      <image:title>{{__('portrait_picture_of', ['nickname' => $pornstar->nickname])}}</image:title>
+   </image:image>
+   @endforeach
+</url>
+
+
+<!-- movies -->
+<url>
+   <loc>{{route('movies.new')}}</loc>
+   <changefreq>daily</changefreq>
+</url>
+
+<url>
+   <loc>{{route('movies.the-most-popular')}}</loc>
+   <changefreq>monthly</changefreq>
+</url>
+
+
+@foreach($movies as $movie)
+<url>
+   <loc>{{$movie->getLink()}}</loc>
+   <changefreq>monthly</changefreq>
+</url>
+@endforeach
 </urlset> 
